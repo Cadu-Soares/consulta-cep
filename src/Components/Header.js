ce0a98ee292b1d7549931b90e1ec72d16a40cbf7
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-27 23:50:09 -0300

    indentação header - prettier/eslint

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,35 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import '../Styles/Header.css'
+import React, { useState, useEffect } from "react";
+import "../Styles/Header.css";
 
-const Header = () =>{
-  
-  const [showMenu, setShowMenu] = useState(false)
-  const [display, setDisplay] = useState('none')
-  
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [display, setDisplay] = useState("none");
+
   useEffect(() => {
-    setDisplay((state) => showMenu ? 'flex' : 'none');
+    setDisplay((state) => (showMenu ? "flex" : "none"));
   }, [showMenu]);
 
   return (
-      <>
-          <div className='menu'>
-            <a className='hamburger-toggle' onClick={ e => setShowMenu(state => !state)}>
-            </a>
-            <br/>
-            <aside  className="menu-area" style={{display:display}} >
-              <ul>
-                <li>Voltar ao menu</li>
-                <li>CLique e Retire</li>
-                <li>Locker</li>
-                <li>Agência Modular</li>
-                <li>Veja Também</li>
-              </ul>
-          </aside>
-          </div>
-
-
-      </>
-    )
-  }
+    <>
+      <div className="menu">
+        <a
+          className="hamburger-toggle"
+          onClick={(e) => setShowMenu((state) => !state)}
+        />
+        <br />
+        <aside className="menu-area" style={{ display: display }}>
+          <ul>
+            <li>Voltar ao menu</li>
+            <li>CLique e Retire</li>
+            <li>Locker</li>
+            <li>Agência Modular</li>
+            <li>Veja Também</li>
+          </ul>
+        </aside>
+      </div>
+    </>
+  );
+};
 
 export default Header;
